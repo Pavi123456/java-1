5a663be66c5539c75c45d7417ce5186a88e26597
Rahini8 <36723038+Rahini8@users.noreply.github.com>
2018-07-30 15:51:46 +0530

    Create count.java

---
+++ count.java
@@ -0,0 +1,15 @@
+import java.io.*;
+public class count
+{
+public static void main(String args[])
+{
+Scanner sc = new Scanner(System.in);
+System.out.println("Please enter a string");
+String input = sc.nextLine();
+System.out.println(input);
+String str = input;
+String[] myString = str.split(" ");
+int length = str.length();
+System.out.println(length);
+}
+}
