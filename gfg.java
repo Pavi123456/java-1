6f73894dac6a83b484a84c48b96e2da7bd9cc17d
Rahini8 <36723038+Rahini8@users.noreply.github.com>
2018-08-30 14:39:28 +0530

    Create gfg.java

---
+++ gfg.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class GFG
+{
+public Static void main(String [] args)
+{
+Scanner in=new Scanner(System.in);
+int n=in.nextInt();
+int c=2*n;
+int count=0;
+for(int i=0,j=1;i<c && j<c;i+=2,j+=2)
+{
+System.out.println(i+" "+j);
+count++;
+}
+System.out.println(count);
+}
